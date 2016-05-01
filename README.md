4a2beefddcfaa365d0d9991a2457c2d5a1afe151
Spencer McIntyre <zeroSteiner@gmail.com>
2016-04-30 22:55:38 -0400

    Add a readme file with basic information

---
+++ README.md
@@ -0,0 +1,18 @@
+![alt text](https://github.com/securestate/king-phisher/raw/master/data/king-phisher-logo.png "King Phisher")
+
+# King Phisher Plugins
+Plugins for the King Phisher Phishing Campaign Toolkit.
+
+These files are provided for use with [King Phisher][king-phisher-repo].
+
+## Installation
+Client plugins can be placed in the `$HOME/.config/king-phisher/plugins`
+directory, then loaded and enabled with the plugin manager.
+
+## License
+King Phisher Templates are released under the BSD 3-clause license, for more
+details see the [LICENSE][license-file] file.
+
+[king-phisher-repo]: https://github.com/securestate/king-phisher
+[king-phisher-wiki]: https://github.com/securestate/king-phisher/wiki
+[license-file]: https://github.com/securestate/king-phisher-templates/blob/master/LICENSE
---
+++ client/spell_check.py
@@ -0,0 +1,29 @@
+import king_phisher.client.plugins as plugins
+
+import gi
+gi.require_version('GtkSpell', '3.0')
+from gi.repository import GtkSpell
+
+class Plugin(plugins.ClientPlugin):
+	authors = ['Spencer McIntyre']
+	title = 'Spell Check'
+	description = """
+	Add spell check capabilities to the message editor. This requires GtkSpell
+	to be available with the correct Python GObject Introspection bindings.
+	After being loaded, the language can be changed from the default of en_US
+	via the context menu (available when right clicking in the text view).
+	"""
+	homepage = 'https://github.com/securestate/king-phisher'
+	def initialize(self):
+		self.checker = GtkSpell.Checker()
+		self.checker.set_language(self.config.get('language', 'en_US'))
+
+		window = self.application.main_window
+		mailer_tab = window.tabs['mailer']
+		edit_tab = mailer_tab.tabs['edit']
+		self.checker.attach(edit_tab.textview)
+		return True
+
+	def finalize(self):
+		self.config['language'] = self.checker.get_language()
+		self.checker.detach()
