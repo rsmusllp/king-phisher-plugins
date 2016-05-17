6b8c6aca1a71cedaf84d832a403fb6301f169ffb
Spencer McIntyre <zeroSteiner@gmail.com>
2016-05-17 18:51:55 -0400

    Refactor the hello_world plugin for readability

--- client/hello_world.py
+++ client/hello_world.py
@@ -10,8 +10,30 @@ class Plugin(plugins.ClientPlugin):
 	"""
 	homepage = 'https://github.com/securestate/king-phisher-plugins'
 	options = [
-		plugins.ClientOptionBoolean('validiction', 'Whether or not this plugin say good bye.', default=True, display_name='Say Good Bye'),
-		plugins.ClientOptionString('name', 'The name to which to say goodbye.', default='Alice Liddle', display_name='Your Name')
+		plugins.ClientOptionBoolean(
+			'validiction',
+			'Whether or not this plugin say good bye.',
+			default=True,
+			display_name='Say Good Bye'
+		),
+		plugins.ClientOptionString(
+			'name',
+			'The name to which to say goodbye.',
+			default='Alice Liddle',
+			display_name='Your Name'
+		),
+		plugins.ClientOptionInteger(
+			'some_number',
+			'An example number option.',
+			default=1337,
+			display_name='A Number'
+		),
+		plugins.ClientOptionPort(
+			'tcp_port',
+			'The TCP port to connect to.',
+			default=80,
+			display_name='Connection Port'
+		)
 	]
 	def initialize(self):
 		print('Hello World!')
@@ -24,4 +46,7 @@ class Plugin(plugins.ClientPlugin):
 	def signal_exit(self, app):
 		if not self.config['validiction']:
 			return
-		gui_utilities.show_dialog_info("Good bye {0}!".format(self.config['name']), app.get_active_window())
+		gui_utilities.show_dialog_info(
+			"Good bye {0}!".format(self.config['name']),
+			app.get_active_window()
+		)
