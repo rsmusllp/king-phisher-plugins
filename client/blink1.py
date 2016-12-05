3de195e02faeccd4e01db0822a23e4bb39d292b8
Spencer McIntyre <zeroSteiner@gmail.com>
2016-12-05 11:59:12 -0500

    Add the logic to turn the blink1 off

--- client/blink1.py
+++ client/blink1.py
@@ -2,6 +2,8 @@ import king_phisher.client.gui_utilities as gui_utilities
 import king_phisher.client.plugins as plugins
 import king_phisher.client.server_events as server_events
 
+from gi.repository import GLib
+
 try:
 	from blink1 import blink1
 	import usb.core
@@ -40,6 +42,7 @@ class Plugin(plugins.ClientPlugin):
 				'Unable to find the Blink(1) device.'
 			)
 			return False
+		self._gsrc_id = None
 		if self.application.server_events is None:
 			self.signal_connect('server-connected', lambda app: self._connect_server_events())
 		else:
@@ -52,12 +55,21 @@ class Plugin(plugins.ClientPlugin):
 		self._blink1 = None
 
 	def _blink1_set_color(self, color):
-		self._blink1.fade_to_color(250, color)
+		self._blink1.fade_to_color(375, color)
+		if color != 'black':
+			if self._gsrc_id is not None:
+				GLib.source_remove(self._gsrc_id)
+			self._gsrc_id = GLib.timeout_add(1625, self._blink1_off)
 		self._color = color
 
 	def _blink1_off(self):
 		self._blink1_set_color('black')
 		self._color = None
+
+	def _blink1_off_timeout(self):
+		self._gsrc_id = None
+		self._blink1_off()
+		return False
 
 	def _connect_server_events(self):
 		self.signal_connect_server_event(
