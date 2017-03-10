e257c8efa48f5b6d0ccb730eb8b6e6183748852d
Spencer McIntyre <zeroSteiner@gmail.com>
2017-03-10 12:02:01 -0500

    Add a new TOTP self enrollment client plugin

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ![alt text](https://github.com/securestate/king-phisher/raw/master/data/king-phisher-logo.png "King Phisher")
-<!-- generated at: 2017-02-15 19:27:43 -->
+<!-- generated at: 2017-03-10 17:02:38 -->
 # King Phisher Plugins
 Plugins to extend the [King Phisher][king-phisher-repo] Phishing Campaign
 Toolkit. For more information regarding King Phisher, see the project's
@@ -12,6 +12,7 @@ Toolkit. For more information regarding King Phisher, see the project's
 | [Clockwork SMS][c_clockwork_sms] | Send SMS messages using the Clockwork SMS API's email gateway. While enabled, this plugin will automatically update phone numbers into email addresses for sending using the service. |
 | [Upload KPM][c_kpm_export_on_send] | Saves a KPM file to the King Phisher server when sending messages. The user must have write permissions to the specified directories. Both the "Local Directory" and "Remote Directory" options can use the variables that are available for use in message templates. |
 | [Generate PDF][c_pdf_generator] | Generates a PDF file with a link which includes the campaign URL with the individual message_id required to track individual visits to a website. Visit https://github.com/y4utj4/pdf_generator for example template files to use with this plugin. |
+| [TOTP Self Enrollment][c_totp_enrollment] | This plugin allows users to manager the two factor authentication settings on their account. This includes setting a new and removing an existing TOTP secret. The two factor authentication used by King Phisher is compatible with free mobile applications such as Google Authenticator. |
 | [Hello World!][c_hello_world] | A 'hello world' plugin to serve as a basic template and demonstration. This plugin will display a message box when King Phisher exits. |
 | [Save KPM On Exit][c_kpm_export_on_exit] | Prompt to save the message data as a KPM file when King Phisher exits. |
 | [Blink(1) Notifications][c_blink1] | A plugin which will flash a Blink(1) peripheral based on campaign events such as when a new visit is received or new credentials have been submitted. |
@@ -25,6 +26,7 @@ Toolkit. For more information regarding King Phisher, see the project's
 | [Pushbullet Notifications][s_pushbullet_notifications] | A plugin that uses Pushbullet's API to send push notifications on new website visits and submitted credentials. |
 | [Hello World!][s_hello_world] | A 'hello world' plugin to serve as a basic template and demonstration. This plugin will log simple messages to show that it is functioning. |
 | [XMPP Notifications][s_xmpp_notifications] | A plugin which pushes notifications regarding the King Phisher server to a specified XMPP server. |
+| [IFTTT Campaign Success Notification][s_ifttt_on_campaign_success] | A plugin that will publish an event to a specified IFTTT Maker channel when a campaign has been deemed 'successful'. |
 
 ## Plugin Installation
 ### Client Plugin Installation
@@ -49,6 +51,7 @@ details see the [LICENSE][license-file] file.
 [c_clockwork_sms]: https://github.com/securestate/king-phisher-plugins/blob/master/client/clockwork_sms.py
 [c_kpm_export_on_send]: https://github.com/securestate/king-phisher-plugins/blob/master/client/kpm_export_on_send.py
 [c_pdf_generator]: https://github.com/securestate/king-phisher-plugins/blob/master/client/pdf_generator.py
+[c_totp_enrollment]: https://github.com/securestate/king-phisher-plugins/blob/master/client/totp_enrollment.py
 [c_hello_world]: https://github.com/securestate/king-phisher-plugins/blob/master/client/hello_world.py
 [c_kpm_export_on_exit]: https://github.com/securestate/king-phisher-plugins/blob/master/client/kpm_export_on_exit.py
 [c_blink1]: https://github.com/securestate/king-phisher-plugins/blob/master/client/blink1.py
@@ -58,3 +61,4 @@ details see the [LICENSE][license-file] file.
 [s_pushbullet_notifications]: https://github.com/securestate/king-phisher-plugins/blob/master/server/pushbullet_notifications.py
 [s_hello_world]: https://github.com/securestate/king-phisher-plugins/blob/master/server/hello_world.py
 [s_xmpp_notifications]: https://github.com/securestate/king-phisher-plugins/blob/master/server/xmpp_notifications.py
+[s_ifttt_on_campaign_success]: https://github.com/securestate/king-phisher-plugins/blob/master/server/ifttt_on_campaign_success.py
---
+++ client/totp_enrollment.py
@@ -0,0 +1,134 @@
+import io
+import os
+
+import king_phisher.client.plugins as plugins
+import king_phisher.client.gui_utilities as gui_utilities
+
+from gi.repository import Gtk
+from gi.repository import GdkPixbuf
+import pyotp
+
+try:
+	import qrcode
+except ImportError:
+	has_qrcode = False
+else:
+	has_qrcode = True
+
+gtk_builder_file = os.path.splitext(__file__)[0] + '.ui'
+
+class Plugin(plugins.ClientPlugin):
+	authors = ['Spencer McIntyre']
+	title = 'TOTP Self Enrollment'
+	description = """
+	This plugin allows users to manager the two factor authentication settings
+	on their account. This includes setting a new and removing an existing TOTP
+	secret. The two factor authentication used by King Phisher is compatible
+	with free mobile applications such as Google Authenticator.
+	"""
+	homepage = 'https://github.com/securestate/king-phisher-plugins'
+	req_min_version = '1.7.0b2'
+	req_packages = {
+		'qrcode': has_qrcode
+	}
+	def initialize(self):
+		if not os.access(gtk_builder_file, os.R_OK):
+			gui_utilities.show_dialog_error(
+				'Plugin Error',
+				self.application.get_active_window(),
+				"The GTK Builder data file ({0}) is not available.".format(os.path.basename(gtk_builder_file))
+			)
+			return False
+		self.menu_items = {}
+		self.add_submenu('Tools > TOTP Self Enrollment')
+		self.menu_items['setup'] = self.add_menu_item('Tools > TOTP Self Enrollment > Setup', self.enrollment_setup)
+		self.menu_items['remove'] = self.add_menu_item('Tools > TOTP Self Enrollment > Remove', self.enrollment_remove)
+		return True
+
+	def check_totp(self, _, window, entry, new_otp, this_user):
+		if not new_otp.verify(entry.get_text().strip()):
+			gui_utilities.show_dialog_warning(
+				'Incorrect TOTP',
+				self.application.get_active_window(),
+				'The specified TOTP code is invalid. Make sure your time\n' +\
+				'is correct, rescan the QR code and try again.'
+			)
+			return
+		this_user.otp_secret = new_otp.secret
+		this_user.commit()
+		gui_utilities.show_dialog_info(
+			'TOTP Enrollment',
+			self.application.get_active_window(),
+			'Successfully set the TOTP secret. Your account is now enrolled\n' +\
+			'in two factor authentication. You will be prompted to enter the\n' +\
+			'value the next time you login.'
+		)
+		window.destroy()
+
+	def enrollment_remove(self, _):
+		rpc = self.application.rpc
+		this_user = rpc.remote_table_row('users', rpc.username)
+		if this_user.otp_secret is None:
+			gui_utilities.show_dialog_info(
+				'Not Enrolled',
+				self.application.get_active_window(),
+				'This account is not currently enrolled in two factor\n' +\
+				'authentication. There are no changes to make.'
+			)
+			return
+		remove = gui_utilities.show_dialog_yes_no(
+			'Already Enrolled',
+			self.application.get_active_window(),
+			'Are you sure you want to unenroll in TOTP? This will remove\n' +\
+			'two factor authentication on your account.'
+		)
+		if not remove:
+			return
+		this_user.otp_secret = None
+		this_user.commit()
+		gui_utilities.show_dialog_info(
+			'TOTP Unenrollment',
+			self.application.get_active_window(),
+			'Successfully removed the TOTP secret. Your account is now unenrolled\n' +\
+			'in two factor authentication. You will no longer be prompted to enter\n' +\
+			'the value when you login.'
+		)
+
+	def enrollment_setup(self, _):
+		rpc = self.application.rpc
+		this_user = rpc.remote_table_row('users', rpc.username)
+		if this_user.otp_secret is not None:
+			reset = gui_utilities.show_dialog_yes_no(
+				'Already Enrolled',
+				self.application.get_active_window(),
+				'This account is already enrolled in TOTP,\nreset the existing TOTP token?'
+			)
+			if not reset:
+				return
+		new_otp = pyotp.TOTP(pyotp.random_base32())
+		profisoning_uri = new_otp.provisioning_uri(rpc.username + '@' + rpc.host) + '&issuer=King%20Phisher'
+		bytes_io = io.BytesIO()
+		qrcode_ = qrcode.make(profisoning_uri).get_image()
+		qrcode_.save(bytes_io, 'PNG')
+		pixbuf_loader = GdkPixbuf.PixbufLoader.new()
+		pixbuf_loader.write(bytes_io.getvalue())
+		pixbuf_loader.close()
+		pixbuf = pixbuf_loader.get_pixbuf()
+
+		self.logger.debug('loading gtk builder file from: ' + gtk_builder_file)
+		builder = Gtk.Builder()
+		builder.add_from_file(gtk_builder_file)
+		window = builder.get_object('TOTPEnrollment.window')
+		window.set_transient_for(self.application.get_active_window())
+
+		self.application.add_window(window)
+
+		image = builder.get_object('TOTPEnrollment.image_qrcode')
+		image.set_from_pixbuf(pixbuf)
+
+		button_check = builder.get_object('TOTPEnrollment.button_check')
+		entry_totp = builder.get_object('TOTPEnrollment.entry_totp')
+		button_check.connect('clicked', self.check_totp, window, entry_totp, new_otp, this_user)
+		entry_totp.connect('activate', self.check_totp, window, entry_totp, new_otp, this_user)
+
+		window.show_all()
