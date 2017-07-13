b57fa0052636b218da913891db1db4102046cb2a
y4utj4@github.com <jschoeneman@outlook.com>
2017-07-13 09:44:19 -0400

    fixed some syntax issues addressed by @fallenwolf

--- client/domain_check.py
+++ client/domain_check.py
@@ -4,11 +4,11 @@ import king_phisher.client.gui_utilities as gui_utilities
 from socket import getaddrinfo
 
 try:
-	import whois
+    import whois
 except ImportError:
-	has_pywhois = False
+    has_pywhois = False
 else:
-	has_pywhois = True
+    has_pywhois = True
 
 class Plugin(plugins.ClientPlugin):
     authors = ['Jeremy Schoeneman']  # the plugins author
@@ -21,12 +21,13 @@ class Plugin(plugins.ClientPlugin):
     version = '1.0'            # (optional) specify this plugin's version
     # this is the primary plugin entry point which is executed when the plugin is enabled
     req_packages = {
-		'pywhois': has_pywhois
-	}
+        'pywhois': has_pywhois
+    }
 
     def initialize(self):
         mailer_tab = self.application.main_tabs['mailer']
         self.signal_connect('send-precheck', self.signal_check_domain, gobject=mailer_tab)
+        return 
 
     def signal_check_domain(self, _):
         email = str(self.application.config['mailer.source_email'])
@@ -38,19 +39,19 @@ class Plugin(plugins.ClientPlugin):
                 try:
                     info = whois.whois(domain)
                     self.logger.info('email domain valid')
-                    gui_utilities.show_dialog_info(
-                    'Email Domain Valid',
-                    self.application.get_active_window(),
-                    'Domain registered to: ' + info.name + '\n',
-                    'Name Servers: ' + info.name_servers + '\n',
-                    'Contact Email: ' + info.emails
+                        gui_utilities.show_dialog_info(
+                        'Email Domain Valid',
+                        self.application.get_active_window(),
+                        'Domain registered to: ' + info.name + '\n',
+                        'Name Servers: ' + info.name_servers + '\n',
+                        'Contact Email: ' + info.emails
                     )
                 except Exception as err:
                     self.logger.info('whois lookup unavailable')
                     gui_utilities.show_dialog_info(
-                    'Email Domain Valid',
-                    self.application.get_active_window(),
-                    'Your domain is valid, however whois lookup failed.'
+                        'Email Domain Valid',
+                        self.application.get_active_window(),
+                        'Your domain is valid, however whois lookup failed.'
                     )
             else:
                 self.logger.info('email domain valid')
