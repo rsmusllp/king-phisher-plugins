39acb1181151fc13e5a2131a73eaa145b830e571
Spencer McIntyre <zeroSteiner@gmail.com>
2018-03-06 18:02:27 -0500

    Refactor the domain_check plugin

--- client/domain_check.py
+++ client/domain_check.py
@@ -1,7 +1,7 @@
+import socket
+
 import king_phisher.client.plugins as plugins
 import king_phisher.client.gui_utilities as gui_utilities
-
-from socket import getaddrinfo
 
 try:
 	import whois
@@ -14,7 +14,8 @@ class Plugin(plugins.ClientPlugin):
 	authors = ['Jeremy Schoeneman']
 	title = 'Domain Validator'
 	description = """
-	Checks to see if a domain can be resolved. Good for email spoofing and
+	Checks to see if a domain can be resolved and then looks up the WHOIS
+	information for it. Good for email spoofing and
 	bypassing some spam filters.
 	"""
 	homepage = 'https://github.com/securestate/king-phisher-plugins'
@@ -24,35 +25,46 @@ class Plugin(plugins.ClientPlugin):
 	}
 	def initialize(self):
 		mailer_tab = self.application.main_tabs['mailer']
-		self.signal_connect('send-precheck', self.signal_check_domain, gobject=mailer_tab)
+		self.signal_connect('send-precheck', self.signal_precheck, gobject=mailer_tab)
 		return True
 
-	def signal_check_domain(self, _):
+	def signal_precheck(self, mailer_tab):
 		email = str(self.application.config['mailer.source_email'])
 		user, _, domain = email.partition('@')
+		self.logger.debug("checking email domain: {0}".format(domain))
+
 		try:
-			self.logger.debug("checking email domain: {0}".format(domain))
-			result = getaddrinfo(domain, None)
-			if result:
-				try:
-					info = whois.whois(domain)
-					self.logger.info('email domain valid')
-					gui_utilities.show_dialog_info(
-						'Email Domain Valid',
-						self.application.get_active_window(),
-						'Domain registered to: ' + info.name + '\n',
-						'Name Servers: ' + info.name_servers + '\n',
-						'Contact Email: ' + info.emails
-					)
-				except Exception as err:
-					self.logger.info('whois lookup unavailable')
-					gui_utilities.show_dialog_info(
-						'Email Domain Valid',
-						self.application.get_active_window(),
-						'Your domain is valid, however whois lookup failed.'
-					)
-			else:
-				self.logger.info("email domain {0} is valid".format(domain))
-		except Exception as err:
-			return gui_utilities.show_dialog_yes_no('Spoofed Email Domain Does not exist, continue?', self.application.get_active_window())
+			socket.getaddrinfo(domain, None)
+		except Exception:
+			response = gui_utilities.show_dialog_yes_no(
+				'Invalid Email Domain',
+				self.application.get_active_window(),
+				'The source email domain does not exist. Continue?'
+			)
+			if not response:
+				return False
+
+		text_insert = mailer_tab.tabs['send_messages'].text_insert
+		text_insert("Checking the WHOIS record for domain '{0}'... ".format(domain))
+		try:
+			info = whois.whois(domain)
+		except Exception as error:
+			text_insert("done, encountered exception: {0}.\n".format(error.__class__.__name__))
+			self.logger.error("whois lookup failed for domain: {0}".format(domain), exc_info=True)
+			response = gui_utilities.show_dialog_info(
+				'Whois Lookup Failed',
+				self.application.get_active_window(),
+				'The domain is valid, however the whois lookup failed. Continue?'
+			)
+			return response
+
+		if any(info.values()):
+			text_insert('done, record found.\nWHOIS Record Overview:\n')
+			text_insert("  Domain registered to: {0!r}\n".format(info.name))
+			if info.name_servers:
+				text_insert("  Name Servers:         {0}\n".format(', '.join(info.name_servers)))
+			if info.emails:
+				text_insert("  Contact Email:        {0}\n".format(info.emails))
+		else:
+			text_insert('done, no record found.\n')
 		return True
