ca9906e8e6b9ef297a43f8c0dcbdcd85d87ba51c
True Demon <truedemon@riseup.net>
2018-04-12 09:48:03 -0400

    Code Cleanup

--- server/alerts_smtp.py
+++ server/alerts_smtp.py
@@ -1,10 +1,11 @@
 import king_phisher.plugins as plugin_opts
 import king_phisher.server.plugins as plugins
 import king_phisher.server.signals as signals
-from smtp2go.core import Smtp2goClient
+from os import environ
+
 
 try:
-	import smtp2go
+	from smtp2go import core as smtp2go
 except ImportError:
 	has_smtp2go = False
 else:
@@ -14,17 +15,13 @@ EXAMPLE_CONFIG = """\
   api_key: <api_key>
 """
 
-class Smtp(Smtp2goClient()):
-	def __init__(self, api_key):
-		self.api_key = api_key		# Inherits from Smtp2goClient to avoid the superclass attempting to retrieve api_key from environment vars
 
 class Plugin(plugins.ServerPlugin):
-	authors = ['Mike Stringer', 'Spencer McIntyre']
+	authors = ['Spencer McIntyre', 'Mike Stringer']
 	title = 'Campaign Alerts: via SMTP2Go'
 	description = """
 	Send campaign alerts via the SMTP2go lib. This requires that users specify
     their email through the King Phisher client to subscribe to notifications.
-    Derived from clockwork_sms plugin by Spencer McIntyre.
 	"""
 	homepage = 'https://github.com/securestate/king-phisher-plugins'
 	options = [
@@ -50,14 +47,20 @@ class Plugin(plugins.ServerPlugin):
 		if not user.email:
 			self.logger.debug("user {0} has no email address specified, skipping SMTP alert".format(user.id))
 			return False
-		api = smtp2go.Smtp2goClient(self.config['api_key'])
+		environ['SMTP2GO_API_KEY'] = self.config['api_key']
+		api = smtp2go.Smtp2goClient()
 		server_email = self.config['server_email']
 
 		message = "{0:,} {1} reached for campaign: {2}".format(count, table.replace('_', ' '), alert_subscription.campaign.name)
-		response = api.send(server_email, user.email, message)
+		response = api.send(
+			sender=server_email,
+			recipients=user.email,
+			subject='Campaign Event: ' + alert_subscription.campaign.name,
+			text=message
+		)
 
 		if not response.success:
 			self.logger.error("received error {0} ({1})".format(response.error_code, response.error_message))
 			return False
-		self.logger.debug("sent an SMS alert to user {0}".format(user.id))
+		self.logger.debug("sent an email alert to user {0}".format(user.id))
 		return True
