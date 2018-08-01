324306b47bd53d223bbb9d099385ebaf2a5383b2
Spencer McIntyre <zeroSteiner@gmail.com>
2018-08-01 11:00:22 -0400

    Minor updates to the message_plaintext plugin

--- client/message_plaintext.py
+++ client/message_plaintext.py
@@ -10,7 +10,7 @@ else:
 
 class Plugin(plugins.ClientPlugin):
 	authors = ['Mike Stringer']
-	classifiers = ['Plugin :: Client :: Email :: Message Plaintext']
+	classifiers = ['Plugin :: Client :: Email :: Spam Evasion']
 	title = 'Message Plaintext'
 	description = """
 	Parse and include a plaintext version of an email based on the HTML version.
@@ -25,39 +25,37 @@ class Plugin(plugins.ClientPlugin):
 	def initialize(self):
 		mailer_tab = self.application.main_tabs['mailer']
 		self.signal_connect('message-create', self.signal_message_create, gobject=mailer_tab)
-
-		if 'message_padding' in self.application.config['plugins']:
-			proceed = gui_utilities.show_dialog_yes_no(
-				'Warning: You are running a conflicting plugin!',
-				self.application.get_active_window(),
-				'The "message_padding" plugin conflicts with "message_plaintext" in such a way ' \
-				+ 'that will cause the message padding to be revealed in the plaintext version ' \
-				+ 'of the email. It is recommended you disable one of these plugins, or append ' \
-				+ 'additional line breaks in the HTML to conceal it.\n\n' \
-				+ 'Do you wish to continue?'
-			)
-			if not proceed:
-				return False
+		self.signal_connect('send-precheck', self.signal_send_precheck, gobject=mailer_tab)
 		return True
 
 	def signal_message_create(self, mailer_tab, target, message):
-		for part in message.walk():
-			if not part.get_content_type().startswith('text/html'):
-				continue
-			html_string = part.payload_string
-
-		try:
-			soup = BeautifulSoup(html_string, 'html.parser')
-		except NameError:
-			self.logger.error('unable to generate plaintext message from HTML')
+		html_part = next((part for part in message.walk() if part.get_content_type().startswith('text/html')), None)
+		if html_part is None:
+			self.logger.error('unable to generate plaintext message from HTML (failed to find text/html part)')
+			return False
+		text_part = next((part for part in message.walk() if part.get_content_type().startswith('text/plain')), None)
+		if text_part is None:
+			self.logger.error('unable to generate plaintext message from HTML (failed to find text/plain part)')
 			return False
 
+		soup = BeautifulSoup(html_part.payload_string, 'html.parser')
 		plaintext_payload_string = soup.get_text()
 		for a in soup.find_all('a', href=True):
 			if 'mailto:' not in a.string:
 				plaintext_payload_string = plaintext_payload_string.replace(a.string, a['href'])
-		for part in message.walk():
-			if not part.get_content_type().startswith('text/plain'):
-				continue
-			part.payload_string = plaintext_payload_string
-			self.logger.debug('plaintext modified from html successfully')
+		text_part.payload_string = plaintext_payload_string
+		self.logger.debug('plaintext modified from html successfully')
+
+	def signal_send_precheck(self, mailer_tab):
+		if 'message_padding' not in self.application.plugin_manager.enabled_plugins:
+			return True
+		proceed = gui_utilities.show_dialog_yes_no(
+			'Warning: You are running a conflicting plugin!',
+			self.application.get_active_window(),
+			'The "message_padding" plugin conflicts with "message_plaintext" in such a way '\
+			+ 'that will cause the message padding to be revealed in the plaintext version '\
+			+ 'of the email. It is recommended you disable one of these plugins, or append '\
+			+ 'additional line breaks in the HTML to conceal it.\n\n' \
+			+ 'Do you wish to continue?'
+		)
+		return proceed
